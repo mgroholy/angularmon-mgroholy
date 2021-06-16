414945ebb28064cd0a33f3ae16e8106c1249d48c
Máté Groholy <mate.groholy@gmail.com>
2021-06-16 16:59:57 +0200

    feat: create pokemon-card component.

---
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ApiServiceService } from 'src/app/services/api-service.service';
+import { Pokemon, PokemonDetails } from 'src/app/Types';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.css'],
+})
+export class PokemonCardComponent implements OnInit {
+  @Input() pokemon: Pokemon;
+  pokemonDetails: PokemonDetails;
+
+  constructor(private apiService: ApiServiceService) {
+    this.pokemon = { name: '', url: '' };
+    this.pokemonDetails = {
+      name: '',
+      height: 0,
+      weight: 0,
+      id: 0,
+      sprites: {
+        front_default: '',
+        other: {
+          official_artwork: {
+            front_default: '',
+          },
+        },
+      },
+    };
+  }
+
+  ngOnInit(): void {
+    this.apiService.getPokemonDetails(this.pokemon.url).subscribe((pokemon) => {
+      this.pokemonDetails = pokemon;
+      console.log(pokemon);
+    });
+  }
+}
